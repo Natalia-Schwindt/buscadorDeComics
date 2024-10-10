b3e0beb2ef00ea45528a5b730f8f4857fba707a1
Natalia-Schwindt <naty8014@gmail.com>
2024-10-10 20:33:01 -0300

    agregando autenticacion api

---
+++ index.js
@@ -0,0 +1,7 @@
+// Autenticación
+const urlApi = "https://gateway.marvel.com";
+const parametrosAutenticacion = `?ts=${TIMESTAMP}&apikey=${API_KEY}&hash=${API_HASH}`;
+
+// URLs de Personajes y Comics
+const urlPersonajes = "/v1/public/characters";
+const urlComics = "/v1/public/comics";
